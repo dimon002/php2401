1a02c79cf6b66ee710585877a1631da00e413021
Dieter Meling <Meling.d@gmx.de>
2024-04-20 12:53:06 +0200

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Versuch</title>
+</head>
+<body>
+    <h1>Meine erste Seite</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, incidunt sequi est quae beatae voluptates eos quisquam delectus, vitae, ex quam dolorum non facilis ullam nesciunt! Quaerat, perferendis minus repudiandae id labore illo temporibus repellat doloremque officiis delectus dolorem dicta natus exercitationem provident laborum a quis? Veniam sunt reiciendis doloremque ea mollitia? Cumque neque sed mollitia nobis accusamus provident nisi facilis, rem, ipsam ducimus nulla totam nam, blanditiis praesentium animi! Nesciunt officiis odit voluptatum sunt dolorem perspiciatis repellendus sapiente, tempore facilis, commodi dignissimos fugit voluptates reiciendis tempora quo est modi doloribus itaque unde sint! Voluptatem molestiae quasi ab similique reprehenderit, architecto asperiores. Tempora error numquam, voluptatem enim ducimus sequi ad fugiat corrupti! Quas magni fugiat officiis saepe vitae optio praesentium qui omnis neque numquam tempora, et, atque accusantium temporibus. Aspernatur eum laborum pariatur error reiciendis facere tempore odio ad voluptates incidunt quod minus et atque, voluptas quis sit molestiae deleniti quae libero, fuga recusandae ratione ducimus sint unde? Adipisci odit eos asperiores totam modi nobis voluptatum quaerat pariatur, ad magni cupiditate consectetur, quia aspernatur maiores labore perspiciatis. Quidem voluptatum quibusdam natus, temporibus dolor asperiores, molestias mollitia culpa ullam, ea consequatur? Error autem vitae animi et ducimus ex, sequi ea laborum.</p>
+    <p>
+    <?php 
+        print("Hello World!");
+
+        echo "So geht es auch...Ist aber veraltet";
+
+        phpinfo();
+    ?>
+    </p>
+
+</body>
+</html>
